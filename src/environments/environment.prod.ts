ca2d4a10645b693185d10b2a2b7066aa3bc26c4f
vlad <curator.zog@gmail.com>
2022-04-22 00:50:45 +0300

    addFirebaseHosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDvrWnMxVDlwN-t-oNeqE-PW2I-O81pKHk",
+    authDomain: "ta9-assignment.firebaseapp.com",
+    databaseURL: "https://ta9-assignment-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "ta9-assignment",
+    storageBucket: "ta9-assignment.appspot.com",
+    messagingSenderId: "789794782336",
+    appId: "1:789794782336:web:6a85b469c430fff72a7a65"
+  }
 };
